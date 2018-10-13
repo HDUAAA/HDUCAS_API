2d8276e1360e9aeab7fbf094a8133c61d1a02de8
ChristianSwift <topman@dingnaiwen.xyz>
2018-10-13 20:32:35 +0800

    update

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ class mCAS {
             $user = phpCAS::getUser();
             $_SESSION['user'] = $user;
           	//window.location.href="getUserInfo.php"；
-          	echo "当前用户名：".$user."<br><a href='./index.php?act=logout' target='_self'>退出</a><a href='./getUserInfo.php'>资料</a>";
+          	echo "当前用户名：".$user."<br><a href='./getUserInfo.php'>资料</a><br><a href='./index.php?act=logout' target='_self'>退出</a>";
         }
         else {
             phpCAS::forceAuthentication();
@@ -45,5 +45,5 @@ else if (@$_REQUEST['act'] == 'login') {
 	$cas->CASLogin();
 }
 else {
-	echo "欢迎使用杭电登录态同步测试程序，点此<a href='./index.php?act=login' target='_self'>登陆</a>。";
+	echo "欢迎使用杭电CAS登陆测试程序，点此<a href='./index.php?act=login' target='_self'>登陆</a>。";
 }
